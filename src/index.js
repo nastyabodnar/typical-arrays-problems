d37b0758441c1d9e8e849c0055ed3af9f3610caa
nastyabodnar <bodnar.post@gmail.com>
2020-10-03 20:55:31 +0600

    feat: add max, min and avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
 exports.min = function min (array) {
-  return 0;
-}
-
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+    if (typeof(array) === 'undefined' || array.length === 0) {
+      return 0;
+    } else {
+        let minValue = Math.min(...array);
+        return minValue;
+    }
+  }
+  
+  exports.max = function max (array) {
+    if (typeof(array) === 'undefined' || array.length === 0) {
+      return 0;
+    } else {
+        let maxValue = Math.max(...array);
+        return maxValue;
+    }
+  }
+  
+  exports.avg = function avg (array) {
+    if (typeof(array) === 'undefined' || array.length === 0) {
+      return 0;
+    } else {
+        let averageValue = array.reduce((accumulator, current) => (accumulator + current)) / array.length;
+        return averageValue;
+    }
+  }
